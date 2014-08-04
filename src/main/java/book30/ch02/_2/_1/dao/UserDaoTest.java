30f4241867474fccb33fa44917c6d6e8e6874cfa
Jeong Yong-Jin <takeardor@gmail.com>
2014-08-05 01:30:03 +0900

    add ch02._2._1

---
+++ src/main/java/book30/ch02/_2/_1/dao/UserDaoTest.java
@@ -0,0 +1,52 @@
+package book30.ch02._2._1.dao;
+
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import book30.ch02._2._1.domain.User;
+
+
+public class UserDaoTest {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		//class
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		//xml
+		//ApplicationContext context = new GenericXmlApplicationContext("book30/ch02/_2/_1/applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
+
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("백기선");
+		user.setPassword("married");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + "등록 성공");
+		
+		User user2 = dao.get(user.getId());
+		
+		if(!user.getName().equals(user2.getName())){
+			System.out.println("테스트 실패 (name)");
+		}
+		else if (!user.getPassword().equals(user2.getPassword())) {
+			System.out.println("테스트 실패 (password)");
+		}
+		else {
+			System.out.println("조회 테스트 성공");
+		}
+		
+		/*
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		System.out.println(user2.getId() + "조회 성공");
+		*/
+		
+		
+	}
+
+}
---
+++ src/main/java/book30/ch02/_2/_2/dao/UserDao.java
@@ -0,0 +1,49 @@
+package book30.ch02._2._2.dao;
+
+import java.sql.*;
+
+import javax.sql.DataSource;
+
+import book30.ch02._2._2.domain.User;
+
+public class UserDao {
+	private DataSource dataSource;
+	
+	public void setDataSource(DataSource dataSource){
+		this.dataSource = dataSource;
+	}
+
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Connection c = dataSource.getConnection();
+
+		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+
+		ps.executeUpdate();
+
+		ps.close();
+		c.close();		
+	}
+
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Connection c = dataSource.getConnection();
+		
+		PreparedStatement ps = c.prepareStatement( "select * from users where id = ?");
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+}
