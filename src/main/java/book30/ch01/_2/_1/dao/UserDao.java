49a2756510f0d0754cdec981e8952e514fda9ff4
Youn-Hee Han <yh21.han@gmail.com>
2014-08-06 11:54:06 -0400

    no message

--- src/main/java/book30/ch01/_2/_1/dao/UserDao.java
+++ src/main/java/book30/ch01/_2/_1/dao/UserDao.java
@@ -1,6 +1,10 @@
 package book30.ch01._2._1.dao;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import book30.ch01._2._1.domain.User;
 
