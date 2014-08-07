486b54067b1cba9b628cf2058de06f99cc49420e
Jeong Yong-Jin <takeardor@gmail.com>
2014-08-07 17:55:23 +0900

    add ch02._4, ch02._5

---
+++ src/main/java/book30/ch02/_4/_1/dao/UserDaoTest.java
@@ -0,0 +1,96 @@
+package book30.ch02._4._1.dao;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import book30.ch02._4._1.domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="../applicationContext.xml")
+public class UserDaoTest {
+	@Autowired
+	private ApplicationContext context;
+	//List 2-19
+	//@Autowired
+	//private UserDao dao;
+	
+	private UserDao dao;
+	private User user1;
+	private User user2;
+	private User user3;
+	
+	@Before
+	public void setUp() {
+		this.dao = this.context.getBean("userDao", UserDao.class);
+		System.out.println(this.context);
+		System.out.println(this);
+	}
+
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		//class
+		//ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+	
+		this.user1 = new User("gyumee", "박성철", "springno1");
+		this.user2 = new User("leegw700", "이길원", "springno2");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		User userget1 = dao.get(user1.getId());
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
+		
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
+				
+	}
+	
+	@Test
+	public void count() throws SQLException, ClassNotFoundException {
+				
+		this.user1 = new User("gyumee", "박성철", "springno1");
+		this.user2 = new User("leegw700", "이길원", "springno2");
+		this.user3 = new User("bumjin", "박범진", "springno3");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+	}
+	
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getUserFailure() throws SQLException, ClassNotFoundException {
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.get("unknown_id");
+	}
+}
---
+++ src/main/java/book30/ch02/_5/_2/_2/junit/JUnitTest.java
@@ -0,0 +1,34 @@
+package book30.ch02._5._2._2.junit;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class JUnitTest {
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+	
+	@Test
+	public void test1() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+	}
+	
+	@Test
+	public void test2() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+	}
+	
+	@Test
+	public void test3() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+	}
+	
+
+}
