0dc31ce1ae49b6b8d6eec379782eaf6ec7513788
Youn-Hee Han <yh21.han@gmail.com>
2014-09-17 12:40:09 +0900

    no message

--- src/main/java/book30/ch02/_5/_2/_3/junit/JUnitTest.java
+++ src/main/java/book30/ch02/_5/_2/_3/junit/JUnitTest.java
@@ -7,8 +7,10 @@ import static org.junit.Assert.assertTrue;
 //import static org.junit.matchers.JUnitMatchers.either;
 import static org.hamcrest.CoreMatchers.either;
 
-import static org.junit.matchers.JUnitMatchers.hasItem;
+//import static org.junit.matchers.JUnitMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.junit.Assert.assertThat;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,21 +18,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="junit.xml")
 public class JUnitTest {
-	@Autowired ApplicationContext context;
+	@Autowired 
+	ApplicationContext context;
 	
 	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
-	static ApplicationContext  contextObject = null;
+	static ApplicationContext contextObject = null;
 	
 	@Test
 	public void test1() {
 		assertThat(testObjects, not(hasItem(this)));
 		testObjects.add(this);
+
 		assertThat(contextObject == null || contextObject == this.context, is(true));
 		contextObject = this.context;
 	}
--- src/main/java/book30/ch02/practice/dao/UserDaoTest.java
+++ src/main/java/book30/ch02/practice/dao/UserDaoTest.java
@@ -1,38 +1,30 @@
-package book30.ch02._4._2.dao;
+package book30.ch02.practice.dao;
 
 import java.sql.SQLException;
-
-import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.CoreMatchers.either;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
-import book30.ch02._4._2.domain.User;
-import book30.ch02._5._2._1.junit.JUnitTest;
+import static org.junit.Assert.assertTrue;
+import book30.ch02.practice.dao.UserDao;
+import book30.ch02.practice.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="../test-applicationContext.xml")
-//List 2- 20
-//@DirtiesContext
 public class UserDaoTest {
 	@Autowired
 	private UserDao dao;
+	
+	@Autowired
+	private ApplicationContext context;
 	
 	private User user1;
 	private User user2;
@@ -42,23 +34,20 @@ public class UserDaoTest {
 	
 	@Before
 	public void setUp() {
-		//List 2-23
-		//dao = new UserDao();
-		//DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/testdb", "spring", "book", true);
-		//dao.setDataSource(dataSource);
-		testUserDao = dao;
+		testUserDao = this.dao;
+
+		this.user1 = new User("gyumee", "박성철", "springno1");
+		this.user2 = new User("leegw700", "이길원", "springno2");
+		this.user3 = new User("bumjin", "박범진", "springno3");
 	}
 
 	@Test
 	public void addAndGet() throws SQLException, ClassNotFoundException {
-		assertThat(testUserDao, either(is(nullValue(UserDao.class))).or(is(this.dao)));
-		
-		this.user1 = new User("gyumee", "박성철", "springno1");
-		this.user2 = new User("leegw700", "이길원", "springno2");
+		assertThat(testUserDao, is(this.dao));
+		assertTrue(UserDaoFailTest.testUserDao == null || UserDaoFailTest.testUserDao == UserDaoTest.testUserDao);
 		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
-		
 		
 		dao.add(user1);
 		dao.add(user2);
@@ -71,16 +60,12 @@ public class UserDaoTest {
 		User userget2 = dao.get(user2.getId());
 		assertThat(userget2.getName(), is(user2.getName()));
 		assertThat(userget2.getPassword(), is(user2.getPassword()));
-				
 	}
 	
 	@Test
 	public void count() throws SQLException, ClassNotFoundException {
-		assertThat(testUserDao, either(is(nullValue(UserDao.class))).or(is(this.dao)));
-				
-		this.user1 = new User("gyumee", "박성철", "springno1");
-		this.user2 = new User("leegw700", "이길원", "springno2");
-		this.user3 = new User("bumjin", "박범진", "springno3");
+		assertThat(testUserDao, is(this.dao));
+		assertTrue(UserDaoFailTest.testUserDao == null || UserDaoFailTest.testUserDao == UserDaoTest.testUserDao);
 		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
@@ -95,12 +80,12 @@ public class UserDaoTest {
 		assertThat(dao.getCount(), is(3));
 	}
 	
-	@Test(expected=EmptyResultDataAccessException.class)
-	public void getUserFailure() throws SQLException, ClassNotFoundException {
+	@Test
+	public void compareBean() {
+		assertThat(testUserDao, is(this.dao));
+		assertTrue(UserDaoFailTest.testUserDao == null || UserDaoFailTest.testUserDao == UserDaoTest.testUserDao);
 		
-		dao.deleteAll();
-		assertThat(dao.getCount(), is(0));
-		
-		dao.get("unknown_id");
+		UserDao dao2 = context.getBean("userDao", UserDao.class);
+		assertThat(dao2, is(this.dao));		
 	}
 }
